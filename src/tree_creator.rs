d978b08e78c64f5cff65cb6e3c695504ad0c1dc8
adamski234 <adamski234@protonmail.com>
2020-06-03 17:09:54 +0200

    Stuff I guess

--- src/tree_creator.rs
+++ src/tree_creator.rs
@@ -1,20 +1,26 @@
-pub fn create_ars_tree(ars_string: String) -> Vec<ARSTreeItem> {
-	let words = split_into_keys(ars_string); //Words is a Vec<String> containing first level parsed and split keys
-	let mut top_level_items = Vec::<ARSTreeItem>::new();
-	for word in words.iter() {
-		if word.is_empty() {
-			continue; //Disregard all empty strings
+pub fn create_ars_tree<'a>(ars_string: String) -> ARSTreeItem<'a> {
+	let mut top_level_node = ARSTreeItem {
+		key: ARSStringOrTree::Keys(Vec::new()),
+		parameter: ARSStringOrTree::Text(String::new()),
+		parent_item: None,
+	};
+	let mut current_node = &top_level_node;
+	let mut current_part = &current_node.key;
+	let mut is_parameter = false;
+	let mut index = 0;
+	for current_char in ars_string.chars() {
+		if current_char == '{' {
+			//Handle new opening bracket
+		} else if current_char == '}' {
+			//Handle all closing brackets by adding the bracket and going up a level in the tree
+		} else if current_char == ':' {
+			//Ignore redundant switches
+		} else {
+			//Add the character to the output
 		}
-		let split_parts_of_current_word: Vec<&str> = word.split(':').collect();
-		let key = String::from(split_parts_of_current_word[0]); //First part of the content split by `:` is the key
-		let param = String::from(split_parts_of_current_word[1..].join("")); //The rest of the content is considered to be the parameter
-		let current_part = ARSTreeItem::new(key, param);
-		top_level_items.push(current_part);
+		//Handle unclosed brackets
 	}
-	for item in &mut top_level_items {
-		item.parse_recursive();
-	}
-	return top_level_items;
+	return top_level_node;
 }
 
 /**
@@ -74,12 +80,13 @@ fn split_into_keys(ars_string: String) -> Vec<String> {
 	return words;
 }
 #[derive(Debug)]
-pub struct ARSTreeItem {
-	key: ARSStringOrTree,
-	parameter: ARSStringOrTree,
+pub struct ARSTreeItem<'a> {
+	key: ARSStringOrTree<'a>,
+	parameter: ARSStringOrTree<'a>,
+	parent_item: Option<&'a ARSTreeItem<'a>>,
 }
 
-impl ARSTreeItem {
+impl<'a> ARSTreeItem<'a> {
 	pub fn parse_recursive(&mut self) {
 		if let ARSStringOrTree::Text(text) = &self.key {
 			//Check if the string contains keys and parse it if it does
@@ -101,18 +108,12 @@ impl ARSTreeItem {
 		let chars: Vec<char> = text_to_check.chars().collect();
 		return chars[0] == '{' && chars[chars.len() - 1] == '}';
 	}
-	pub fn new(key: String, param: String) -> ARSTreeItem {
-		return ARSTreeItem {
-			key: ARSStringOrTree::Text(key),
-			parameter: ARSStringOrTree::Text(param),
-		}
-	}
 }
 
 #[derive(Debug)]
-enum ARSStringOrTree {
+enum ARSStringOrTree<'a> {
 	Text(String),
-	Keys(Vec<ARSTreeItem>),
+	Keys(Vec<ARSTreeItem<'a>>),
 }
 
 #[cfg(test)]

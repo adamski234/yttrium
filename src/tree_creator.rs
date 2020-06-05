0188aae618259791d21395e872955c61f2d0bcdc
adamski234 <adamski234@protonmail.com>
2020-06-05 21:52:08 +0200

    Added parameter handling I think

--- src/tree_creator.rs
+++ src/tree_creator.rs
@@ -3,18 +3,34 @@ pub fn create_ars_tree(ars_string: String) -> ARSTreeItem {
 		key: ARSStringOrTree::Text(String::new()),
 		parameter: ARSStringOrTree::Text(String::new()),
 		parent_item: None,
+		is_editing_param: false
 	};
 	let mut current_node = &mut top_level_node; //Reference to the currently edited node
 	let mut current_part = &mut current_node.key; //Reference to the currently edited string
-	let mut is_parameter = false;
 	let mut index = 0;
 	for current_char in ars_string.chars() {
 		if current_char == '{' {
 			//Handle new opening bracket
 		} else if current_char == '}' {
 			//Handle all closing brackets by adding the bracket and going up a level in the tree
+			match current_part {
+				ARSStringOrTree::Text(text) => {
+					//
+				}
+				ARSStringOrTree::Keys(keys) => {
+					//
+				}
+			}
 		} else if current_char == ':' {
 			//Ignore redundant switches
+			if current_node.is_editing_param {
+				if let ARSStringOrTree::Text(text) = current_part {
+					text.push(current_char);
+				}
+			} else {
+				current_node.is_editing_param = true;
+				current_part = &mut current_node.parameter;
+			}
 		} else {
 			//Add the character to the output
 		}
@@ -28,6 +44,7 @@ pub struct ARSTreeItem {
 	key: ARSStringOrTree,
 	parameter: ARSStringOrTree,
 	parent_item: Option<Box<ARSTreeItem>>,
+	is_editing_param: bool,
 }
 
 #[derive(Debug)]

ed79bf471cfb016eab652c322c6287f2dc9434ac
adamski234 <adamski234@protonmail.com>
2020-06-17 18:02:43 +0200

    Easteregg

--- src/tree_creator.rs
+++ src/tree_creator.rs
@@ -1,48 +1,67 @@
 #[allow(clippy::needless_return)]
 #[path = "tokenizer.rs"] mod tokenizer;
 
-pub fn create_ars_tree(ars_string: String) -> Vec<TreeNodeOrString> {
+type Id = usize;
+
+pub fn create_ars_tree(ars_string: String) /*-> Vec<Parameter>*/ {
 	/*
 	How things work:
 	node_list is a flat vector of all nodes in the tree.
 	This way I don't have to work with pointers but rather just vector indices
+	After creating a new node, push it to node_list and use the new index as parent pointer
 	I'll probably want to jump off a bridge after finishing it
 	*/
 	let mut node_list = vec![
-		TreeNodeOrString::String(String::new())
+		TreeNode {
+			key: String::from("top"), //`top` is the top level TreeNode containing all other nodes
+			parameter: Some(Parameter::String(String::new())),
+			is_editing_parameter: true,
+			parent: None
+		}
 	];
 	let mut current_node_index = 0;
 	let tokens = tokenizer::split_into_tokens(ars_string);
 	for token in tokens {
+		use tokenizer::TokenType;
 		match token.token_type {
-			tokenizer::TokenType::OpenBracket => {
+			TokenType::OpenBracket => {
+				if node_list[current_node_index].is_editing_parameter {
+					match &mut node_list[current_node_index].parameter {
+						Some(param) => {
+							//
+						}
+						None => {
+							//
+						}
+					}
+				} else {
+					node_list[current_node_index].key.push_str(&token.text);
+				}
+			}
+			TokenType::CloseBracket => {
 				//
 			}
-			tokenizer::TokenType::CloseBracket => {
+			TokenType::ParameterDelimiter => {
 				//
 			}
-			tokenizer::TokenType::ParameterDelimiter => {
-				//
-			}
-			tokenizer::TokenType::StringLiteral => {
+			TokenType::StringLiteral => {
 				//
 			}
 		}
 	}
-	return node_list;
+	//return node_list;
 }
 
 #[derive(Debug)]
 pub struct TreeNode {
-	key: TreeNodeOrString,
-	parameter: TreeNodeOrString,
+	key: String, //Cannot be ars code, as it would require getting opcodes on the fly. Could work with an interpreter tho
+	parameter: Option<Parameter>, //String for literals, Nodes for variable values
 	is_editing_parameter: bool,
-	parent: Option<usize>, //Pointer, except that it's a vector index instead of a memory address
+	parent: Option<Id>, //Pointer, except that it's a vector index instead of a memory address
 }
 
 #[derive(Debug)]
-pub enum TreeNodeOrString {
-	NodesOrStrings(Vec<TreeNodeOrString>),
-	Node(Box<TreeNode>),
+pub enum Parameter {
+	Nodes(Vec<Id>),
 	String(String)
 }

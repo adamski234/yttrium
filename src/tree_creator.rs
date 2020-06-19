e29312f26a68d8b8c36a3b40c1e06939aef5e053
adamski234 <adamski234@protonmail.com>
2020-06-19 20:15:36 +0200

    Implemented opening new keys

--- src/tree_creator.rs
+++ src/tree_creator.rs
@@ -3,7 +3,7 @@
 
 type Id = usize;
 
-pub fn create_ars_tree(ars_string: String) /*-> Vec<Parameter>*/ {
+pub fn create_ars_tree(ars_string: String) -> Vec<TreeNode> {
 	/*
 	How things work:
 	node_list is a flat vector of all nodes in the tree.
@@ -11,7 +11,7 @@ pub fn create_ars_tree(ars_string: String) /*-> Vec<Parameter>*/ {
 	After creating a new node, push it to node_list and use the new index as parent pointer
 	I'll probably want to jump off a bridge after finishing it
 	*/
-	let mut node_list = vec![
+	let mut top_node_list = vec![
 		TreeNode {
 			key: String::from("top"), //`top` is the top level TreeNode containing all other nodes
 			parameter: Some(Parameter::String(String::new())),
@@ -25,17 +25,59 @@ pub fn create_ars_tree(ars_string: String) /*-> Vec<Parameter>*/ {
 		use tokenizer::TokenType;
 		match token.token_type {
 			TokenType::OpenBracket => {
-				if node_list[current_node_index].is_editing_parameter {
-					match &mut node_list[current_node_index].parameter {
+				if top_node_list[current_node_index].is_editing_parameter {
+					let top_node_list_size = top_node_list.len(); //satisfying the borrow checker
+					match &mut top_node_list[current_node_index].parameter {
 						Some(param) => {
-							//
+							match param {
+								Parameter::Nodes(child_nodes) => {
+									let new_node = TreeNode {
+										key: String::new(),
+										parameter: None,
+										is_editing_parameter: false,
+										parent: Some(current_node_index),
+									};
+									child_nodes.push(top_node_list_size);
+									top_node_list.push(new_node);
+									current_node_index = top_node_list_size + 1;
+								}
+								Parameter::String(text) => {
+									let node_from_text = TreeNode {
+										key: String::from("literal"),
+										parameter: Some(Parameter::String(text.to_string())),
+										is_editing_parameter: true,
+										parent: Some(current_node_index)
+									};
+									let new_node = TreeNode {
+										key: String::new(),
+										parameter: None,
+										is_editing_parameter: false,
+										parent: Some(current_node_index),
+									};
+									let child_nodes = vec![ //Children IDs
+										top_node_list_size,
+										top_node_list_size + 1,
+									];
+									top_node_list.push(node_from_text);
+									top_node_list.push(new_node);
+									top_node_list[current_node_index].parameter = Some(Parameter::Nodes(child_nodes));
+									current_node_index = top_node_list_size + 1;
+								}
+							}
 						}
 						None => {
-							//
+							let new_node = TreeNode {
+								key: String::new(),
+								parameter: None,
+								is_editing_parameter: false,
+								parent: Some(current_node_index),
+							};
+							top_node_list[current_node_index].parameter = Some(Parameter::Nodes(vec![top_node_list_size]));
+							top_node_list.push(new_node);
 						}
 					}
 				} else {
-					node_list[current_node_index].key.push_str(&token.text);
+					top_node_list[current_node_index].key.push_str(&token.text);
 				}
 			}
 			TokenType::CloseBracket => {
@@ -49,7 +91,7 @@ pub fn create_ars_tree(ars_string: String) /*-> Vec<Parameter>*/ {
 			}
 		}
 	}
-	//return node_list;
+	return top_node_list;
 }
 
 #[derive(Debug)]

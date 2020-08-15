1c2d460293d9ed18bdffac615c3e5df26306d527
adamski234 <adamski234@protonmail.com>
2020-08-15 19:27:29 +0200

    Added tests

--- src/errors_and_warns.rs
+++ src/errors_and_warns.rs
@@ -48,3 +48,66 @@ pub fn check_for_errors(nodes: &Vec<tree_creator::TreeNode>) -> Option<Error> {
 	}
 	return None;
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn no_parameter() {
+		use crate::tree_creator::{TreeNode, NodeEntryType::Unconditional, Parameter, UnconditionalNodeEntry};
+		let input = vec![TreeNode {
+            inner_node: Unconditional(
+                UnconditionalNodeEntry {
+                    key: String::from("abc"),
+                    parameter: Some(
+                        Parameter::String(
+                            String::new(),
+                        ),
+                    ),
+                    is_editing_parameter: true,
+                },
+            ),
+            parent: None
+		}];
+		let output = check_for_errors(&input);
+		assert_eq!(output, Some(Error::EmptyParameter));
+	}
+	#[test]
+	fn no_condition() {
+		use crate::tree_creator::{TreeNode, NodeEntryType::Conditional, Parameter, ConditionalNodeEntry, CurrentlyEditedPartOfConditional::Condition};
+		let input = vec![TreeNode {
+			inner_node: Conditional(ConditionalNodeEntry {
+				condition: Parameter::String(
+					String::new(),
+				),
+				if_condition_true: Parameter::String(
+					String::new(),
+				),
+				if_condition_false: None,
+				currently_edited_part: Condition,
+			}),
+			parent: None,
+		}];
+		let output = check_for_errors(&input);
+		assert_eq!(output, Some(Error::EmptyCondition));
+	}
+	#[test]
+	fn no_condition_true() {
+		use crate::tree_creator::{TreeNode, NodeEntryType::Conditional, Parameter, ConditionalNodeEntry, CurrentlyEditedPartOfConditional::Condition};
+		let input = vec![TreeNode {
+			inner_node: Conditional(ConditionalNodeEntry {
+				condition: Parameter::String(
+					String::from("abc"),
+				),
+				if_condition_true: Parameter::String(
+					String::new(),
+				),
+				if_condition_false: None,
+				currently_edited_part: Condition,
+			}),
+			parent: None,
+		}];
+		let output = check_for_errors(&input);
+		assert_eq!(output, Some(Error::EmptyIfConditionTrue));
+	}
+}

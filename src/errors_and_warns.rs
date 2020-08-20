c34c9e8cf00eab7ba6101682ce34a43dfdaa2621
adamski234 <adamski234@protonmail.com>
2020-08-20 20:03:14 +0200

    Remade errors to use the new system

--- src/errors_and_warns.rs
+++ src/errors_and_warns.rs
@@ -3,9 +3,8 @@ use crate::tree_creator;
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum Error {
-	NoParameterSupplied,
-	EmptyCondition,
-	EmptyIfConditionTrue,
+	NotEnoughParameters,
+	TooManyParameters,
 	BracketsInCond,
 	ParameterDelimAfterCondFalse,
 	EmptyParameter,
@@ -19,28 +18,12 @@ pub enum Warning {
 
 pub fn check_for_errors(nodes: &Vec<tree_creator::TreeNode>) -> Option<Error> {
 	for node in nodes {
-		//TODO check if key requires a parameter for `Error::NoParameterSupplied`
-		match &node.inner_node {
-			tree_creator::NodeEntryType::Unconditional(inner) => {
-				if inner.is_editing_parameter {
-					if let Some(param) = &inner.parameter {
-						if let tree_creator::Parameter::String(text) = param {
-							if text.is_empty() {
-								return Some(Error::EmptyParameter);
-							}
-						}
-					}
-				}
-			}
-			tree_creator::NodeEntryType::Conditional(inner) => {
-				if let tree_creator::Parameter::String(text) = &inner.condition {
-					if text.is_empty() {
-						return Some(Error::EmptyCondition);
-					}
-				}
-				if let tree_creator::Parameter::String(text) = &inner.if_condition_true {
-					if text.is_empty() {
-						return Some(Error::EmptyIfConditionTrue);
+		//TODO implement the rest of errors and add tests
+		if node.is_editing_parameter {
+			for param in &node.parameters {
+				if let tree_creator::Parameter::String(string) = param {
+					if string.is_empty() {
+						return Some(Error::EmptyParameter);
 					}
 				}
 			}
@@ -54,60 +37,19 @@ mod tests {
 	use super::*;
 	#[test]
 	fn no_parameter() {
-		use crate::tree_creator::{TreeNode, NodeEntryType::Unconditional, Parameter, UnconditionalNodeEntry};
+		use crate::tree_creator::{TreeNode, Parameter};
 		let input = vec![TreeNode {
-            inner_node: Unconditional(
-                UnconditionalNodeEntry {
-                    key: String::from("abc"),
-                    parameter: Some(
-                        Parameter::String(
-                            String::new(),
-                        ),
-                    ),
-                    is_editing_parameter: true,
-                },
-            ),
-            parent: None
+			key: String::from("abc"),
+			parameters: vec![
+				Parameter::String(
+					String::new(),
+				),
+			],
+			is_editing_parameter: true,
+			edited_parameter: 0,
+			parent: None
 		}];
 		let output = check_for_errors(&input);
 		assert_eq!(output, Some(Error::EmptyParameter));
-	}
-	#[test]
-	fn no_condition() {
-		use crate::tree_creator::{TreeNode, NodeEntryType::Conditional, Parameter, ConditionalNodeEntry, CurrentlyEditedPartOfConditional::Condition};
-		let input = vec![TreeNode {
-			inner_node: Conditional(ConditionalNodeEntry {
-				condition: Parameter::String(
-					String::new(),
-				),
-				if_condition_true: Parameter::String(
-					String::new(),
-				),
-				if_condition_false: None,
-				currently_edited_part: Condition,
-			}),
-			parent: None,
-		}];
-		let output = check_for_errors(&input);
-		assert_eq!(output, Some(Error::EmptyCondition));
-	}
-	#[test]
-	fn no_condition_true() {
-		use crate::tree_creator::{TreeNode, NodeEntryType::Conditional, Parameter, ConditionalNodeEntry, CurrentlyEditedPartOfConditional::Condition};
-		let input = vec![TreeNode {
-			inner_node: Conditional(ConditionalNodeEntry {
-				condition: Parameter::String(
-					String::from("abc"),
-				),
-				if_condition_true: Parameter::String(
-					String::new(),
-				),
-				if_condition_false: None,
-				currently_edited_part: Condition,
-			}),
-			parent: None,
-		}];
-		let output = check_for_errors(&input);
-		assert_eq!(output, Some(Error::EmptyIfConditionTrue));
 	}
 }

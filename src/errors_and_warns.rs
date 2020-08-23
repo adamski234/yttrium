830f4241a365dcbc3da4740624170f6eda8d4ca9
adamski234 <adamski234@protonmail.com>
2020-08-23 21:45:25 +0200

    Made errors respect the parameter count property

--- src/errors_and_warns.rs
+++ src/errors_and_warns.rs
@@ -3,8 +3,7 @@ use crate::tree_creator;
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum Error {
-	NotEnoughParameters,
-	TooManyParameters,
+	WrongAmountOfParameters,
 	BracketsInCond,
 	ParameterDelimAfterCondFalse,
 	EmptyParameter,
@@ -16,14 +15,39 @@ pub enum Warning {
 	UnclosedKeys,
 }
 
-pub fn check_for_errors(nodes: &Vec<tree_creator::TreeNode>) -> Option<Error> {
+pub fn check_for_errors(nodes: &Vec<tree_creator::TreeNode>, keys: &Vec<Box<dyn key_base::Key>>) -> Option<Error> {
 	for node in nodes {
 		//TODO implement the rest of errors and add tests
 		if node.is_editing_parameter {
+			for key in keys {
+				let key_info = key.get_key_info();
+				if key_info.name == node.key {
+					//We have the correct key. Now check the parameter count
+					let mut is_correct = false;
+					for current_count in key_info.parameters_required {
+						if node.edited_parameter + 1 == current_count {
+							is_correct = true;
+							break;
+						}
+					}
+					if !is_correct {
+						return Some(Error::WrongAmountOfParameters);
+					}
+				}
+			}
 			for param in &node.parameters {
 				if let tree_creator::Parameter::String(string) = param {
 					if string.is_empty() {
 						return Some(Error::EmptyParameter);
+					}
+				}
+			}
+		} else {
+			for key in keys {
+				let key_info = key.get_key_info();
+				if key_info.name == node.key {
+					if key_info.parameters_required[0] != 0 {
+						return Some(Error::WrongAmountOfParameters);
 					}
 				}
 			}
@@ -49,7 +73,7 @@ mod tests {
 			edited_parameter: 0,
 			parent: None
 		}];
-		let output = check_for_errors(&input);
+		let output = check_for_errors(&input, &vec![]);
 		assert_eq!(output, Some(Error::EmptyParameter));
 	}
 }

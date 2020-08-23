2ad3d34518296724a75c88eb397313791f2c2028
adamski234 <adamski234@protonmail.com>
2020-08-23 16:21:38 +0200

    Added a basic check for keys

--- src/key_loader.rs
+++ src/key_loader.rs
@@ -1,8 +1,78 @@
 use key_base;
 
 //TODO actually implement loading. This is currently just a placeholder
+//Also, make this a hashmap
 pub fn load_keys(directory: &str) -> Vec<Box<dyn key_base::Key>> {
-	let keys = Vec::new();
+	//let keys = Vec::new();
+	let mut keys = Vec::<Box<dyn key_base::Key>>::new();
+	keys.push(
+		Box::new(Key1 {
+			function: placeholder_fn,
+			info: key_base::KeyInfo {
+				parameters_required: vec![0],
+				name: String::from("abc"),
+				opcode: 0,
+				allowed_key_names: vec![],
+			}
+		})
+	);
+	keys.push(
+		Box::new(Key1 {
+			function: placeholder_fn,
+			info: key_base::KeyInfo {
+				parameters_required: vec![1],
+				name: String::from("def"),
+				opcode: 0,
+				allowed_key_names: vec![],
+			}
+		})
+	);
+	keys.push(
+		Box::new(Key1 {
+			function: placeholder_fn,
+			info: key_base::KeyInfo {
+				parameters_required: vec![0, 1, 3],
+				name: String::from("ghi"),
+				opcode: 0,
+				allowed_key_names: vec![],
+			}
+		})
+	);
+	keys.push(
+		Box::new(Key1 {
+			function: placeholder_fn,
+			info: key_base::KeyInfo {
+				parameters_required: vec![],
+				name: String::from("jkm"),
+				opcode: 0,
+				allowed_key_names: vec![],
+			}
+		})
+	);
+	keys.push(
+		Box::new(Key1 {
+			function: placeholder_fn,
+			info: key_base::KeyInfo {
+				parameters_required: vec![2],
+				name: String::from("ab"),
+				opcode: 0,
+				allowed_key_names: vec![],
+			}
+		})
+	);
+	//Verification and information step
+	let mut to_remove = Vec::new();
+	for (index, key) in keys.iter().enumerate() {
+		let key_info = key.get_key_info();
+		println!("Loaded key {}", key_info.name);
+		if key_info.parameters_required.is_empty() {
+			to_remove.push(index);
+			println!("Key {} had `parameters_required` empty", key_info.name);
+		}
+	}
+	for index in to_remove {
+		keys.remove(index);
+	}
 	return keys;
 }
 
@@ -18,4 +88,7 @@ impl key_base::Key for Key1 {
 	fn get_key_function(&self) -> fn(parameter: &String) -> bool {
 		return self.function;
 	}
+}
+fn placeholder_fn(_param: &String) -> bool {
+	return true;
 }

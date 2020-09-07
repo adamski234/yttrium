bda979ea070dcf026a17cd0c17ee1685d6bc501d
adamski234 <adamski234@protonmail.com>
2020-09-07 20:19:17 +0200

    Made the loader more fault tolerant

--- src/key_loader.rs
+++ src/key_loader.rs
@@ -15,17 +15,33 @@ pub fn load_keys(directory: &str) -> Keys {
 	}
 	let files = fs::read_dir(directory).expect("Key directory did not exist");
 	for file in files {
-		let file = file.expect("Error while reading file");
-		let library = libloading::Library::new(file.path()).expect(&format!("Error while opening library at `{}`", file.path().to_str().unwrap()));
-		unsafe {
-			let creator_function: libloading::Symbol<KeyCreateFunction> = library.get(KEY_CREATE_FUNCTION_NAME).expect(&format!(
-				"Library at `{}` had no {} function",
-				file.path().to_str().unwrap(),
-				String::from_utf8(KEY_CREATE_FUNCTION_NAME.into()).unwrap())
-			);
-			keys.keys.push(Box::from_raw(creator_function()));
+		if file.is_err() {
+			println!("Could not read file due to error:");
+			println!("{}", file.unwrap_err());
+			continue;
 		}
-		keys.libraries.push(library);
+		let file = file.unwrap();
+		let library = libloading::Library::new(file.path());
+		if library.is_err() {
+			println!("Could not open library at `{}` due to error:", file.path().to_str().unwrap());
+			println!("{}", library.unwrap_err());
+			continue;
+		}
+		let library = library.unwrap();
+		unsafe {
+			let creator_function: Result<libloading::Symbol<KeyCreateFunction>, libloading::Error> = library.get(KEY_CREATE_FUNCTION_NAME);
+			if creator_function.is_err() {
+				println!(
+					"Library at `{}` had no {} function",
+					file.path().to_str().unwrap(),
+					String::from_utf8(KEY_CREATE_FUNCTION_NAME.into()).unwrap()
+				);
+				continue;
+			}
+			let creator_function = creator_function.unwrap();
+			keys.keys.push(Box::from_raw(creator_function()));
+			keys.libraries.push(library);
+		}
 	}
 	//Verification and information step
 	let mut to_remove = Vec::new();

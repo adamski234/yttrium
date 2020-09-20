7927180804c98bd8057f45de36e24480e4faf8d1
adamski234 <adamski234@protonmail.com>
2020-09-20 17:59:51 +0200

    Finished the conditional

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -34,9 +34,96 @@ pub fn interpret_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<Stri
 	//Remember: no recursion
 	loop {
 		let mut current_node = &mut interpretable_tree[current_index];
-		if current_node.inner_node.parameters.len() == current_node.interpreted_param {
+		//This is bad coding but I lack creativity to fix this
+		if current_node.inner_node.key == "cond" && current_node.interpreted_param == 1 {
+			//The first parameter has been executed
+			if current_node.returned_values[0].is_empty() || current_node.returned_values[0] == "0" {
+				if current_node.inner_node.parameters.len() == 3 {
+					current_node.interpreted_param = 2;
+				} else {
+					current_index = current_node.inner_node.parent.unwrap();
+					current_node = &mut interpretable_tree[current_index];
+					current_node.returned_subvalues.push(String::from(""));
+				}
+			} else {
+				//To do somewhere in the future: Extract this into a function
+				let current_parameter = &current_node.inner_node.parameters[current_node.interpreted_param];
+				match current_parameter {
+				    tree_creator::Parameter::Nodes(nodes) => {
+						//Handle nodes as parameters
+						if nodes.len() == current_node.returned_subvalues.len() {
+							/* All parameter chunks were ran, now push the result of the parameter to the parent,
+							clear the subvalues, increment the parameter pointer and zero the subparameter pointer
+							*/
+							current_node.finish_subvalue();
+							current_node.interpreted_param += 1;
+						} else {
+							//Handle unfinished parameters
+							current_index = nodes[current_node.interpreted_subparam];
+							current_node.interpreted_subparam += 1;
+						}
+					}
+				    tree_creator::Parameter::String(text) => {
+						//Handle just text as parameter
+						current_node.returned_values.push(text.clone());
+						current_node.interpreted_param += 1;
+						current_node.interpreted_subparam = 0;
+					}
+				}
+			}
+		} else if current_node.inner_node.key == "cond" && current_node.interpreted_param == 2 {
+			if current_node.returned_values[0].is_empty() || current_node.returned_values[0] == "0" {
+				let current_parameter = &current_node.inner_node.parameters[current_node.interpreted_param];
+				match current_parameter {
+				    tree_creator::Parameter::Nodes(nodes) => {
+						//Handle nodes as parameters
+						if nodes.len() == current_node.returned_subvalues.len() {
+							/* All parameter chunks were ran, now push the result of the parameter to the parent,
+							clear the subvalues, increment the parameter pointer and zero the subparameter pointer
+							*/
+							current_node.finish_subvalue();
+							current_node.interpreted_param += 1;
+						} else {
+							//Handle unfinished parameters
+							current_index = nodes[current_node.interpreted_subparam];
+							current_node.interpreted_subparam += 1;
+						}
+					}
+				    tree_creator::Parameter::String(text) => {
+						//Handle just text as parameter
+						current_node.returned_values.push(text.clone());
+						current_node.interpreted_param += 1;
+						current_node.interpreted_subparam = 0;
+					}
+				}
+			} else {
+				//First param is truthy, return the second param
+				let to_return = current_node.returned_values[1].clone();
+				current_index = current_node.inner_node.parent.unwrap();
+				current_node = &mut interpretable_tree[current_index];
+				current_node.returned_subvalues.push(to_return);
+			}
+		} else if current_node.inner_node.key == "cond" && current_node.interpreted_param == 3 {
+			//First param is falsey, return the third param
+			let to_return = current_node.returned_values[1].clone();
+			current_index = current_node.inner_node.parent.unwrap();
+			current_node = &mut interpretable_tree[current_index];
+			current_node.returned_subvalues.push(to_return);
+		} else if current_node.inner_node.parameters.len() == current_node.interpreted_param {
 			//Finished executing all the parameters, now execute the key and return to parent
 			match current_node.inner_node.parent {
+				Some(parent) => {
+					let returned;
+					if current_node.inner_node.key == "ars" {
+						next_rule = Some(current_node.returned_values.join(""));
+						returned = String::new();
+					} else {
+						returned = key_list.get(&current_node.inner_node.key).unwrap().get_key_function()(&current_node.returned_values, environment);
+					}
+					current_index = parent;
+					current_node = &mut interpretable_tree[current_index];
+					current_node.returned_subvalues.push(returned);
+				}
 				None => {
 					//No more keys to interpret, return the result
 					return InterpretationResult {
@@ -44,25 +131,6 @@ pub fn interpret_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<Stri
 						embed: None,
 						next_rule: next_rule,
 					};
-				}
-				Some(parent) => {
-					let returned;
-					if current_node.inner_node.key == "ars" {
-						next_rule = Some(current_node.returned_values.join(""));
-						returned = String::new();
-					} else if current_node.inner_node.key == "cond" {
-						if current_node.returned_values[0] != "1" {
-							//This will not work with cond with no false case
-							returned = current_node.returned_values[2].clone();
-						} else {
-							panic!("`cond` key had the last parameter executed despite the first param being truthy");
-						}
-					} else {
-						returned = key_list.get(&current_node.inner_node.key).unwrap().get_key_function()(&current_node.returned_values, environment);
-					}
-					current_index = parent;
-					current_node = &mut interpretable_tree[current_index];
-					current_node.returned_subvalues.push(returned);
 				}
 			}
 		} else {
@@ -74,9 +142,7 @@ pub fn interpret_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<Stri
 						/* All parameter chunks were ran, now push the result of the parameter to the parent,
 						clear the subvalues, increment the parameter pointer and zero the subparameter pointer
 						*/
-						current_node.returned_values.push(current_node.returned_subvalues.join(""));
-						current_node.returned_subvalues.clear();
-						current_node.interpreted_subparam = 0;
+						current_node.finish_subvalue();
 						current_node.interpreted_param += 1;
 					} else {
 						//Handle unfinished parameters
@@ -94,7 +160,6 @@ pub fn interpret_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<Stri
 		}
 	}
 }
-
 #[derive(Debug)]
 pub struct InterpretationResult {
 	pub message: String,
@@ -112,4 +177,12 @@ struct InterpretableNode {
 	pub returned_values: Vec<String>,
 	pub returned_subvalues: Vec<String>,
 	pub cond_if_false: bool,
+}
+
+impl InterpretableNode {
+	fn finish_subvalue(&mut self) {
+		self.returned_values.push(self.returned_subvalues.join(""));
+		self.returned_subvalues.clear();
+		self.interpreted_subparam = 0;
+	}
 }

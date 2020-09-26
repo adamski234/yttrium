4bf5e1eb6b47d2d49f682f31271145ccaa5636da
adamski234 <adamski234@protonmail.com>
2020-09-26 21:24:01 +0200

    Added all event info

--- key_base/src/events.rs
+++ key_base/src/events.rs
@@ -5,17 +5,17 @@ pub enum EventType {
 	MemberJoin(MemberJoinEventInfo),
 	MemberLeave(MemberLeaveEventInfo),
 	Message(MessageEventInfo),
-	MemberUpdate,
-	RoleCreate,
-	RoleDelete,
-	RoleUpdate,
-	ChannelCreate,
-	ChannelDelete,
-	ChannelUpdate,
-	ServerUpdate,
-	VoiceUpdate,
-	ReactionAdd,
-	ReactionRemove,
+	MemberUpdate(MemberUpdateEventInfo),
+	RoleCreate(RoleCreateEventInfo),
+	RoleDelete(RoleDeleteEventInfo),
+	RoleUpdate(RoleUpdateEventInfo),
+	ChannelCreate(ChannelCreateEventInfo),
+	ChannelDelete(ChannelDeleteEventInfo),
+	ChannelUpdate(ChannelUpdateEventInfo),
+	GuildUpdate(GuildUpdateEventInfo),
+	VoiceUpdate(VoiceUpdateEventInfo),
+	ReactionAdd(ReactionAddEventInfo),
+	ReactionRemove(ReactionRemoveEventInfo),
 }
 
 #[derive(Debug)]
@@ -39,7 +39,7 @@ pub struct MemberJoinEventInfo {
 
 impl MemberJoinEventInfo {
 	pub fn new(user_id: String) -> Self { 
-		return Self { user_id }
+		return Self { user_id };
 	}
 }
 
@@ -50,6 +50,132 @@ pub struct MemberLeaveEventInfo {
 
 impl MemberLeaveEventInfo {
 	pub fn new(user_id: String) -> Self { 
-		return Self { user_id }
+		return Self { user_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct MemberUpdateEventInfo {
+	pub user_id: String,
+}
+
+impl MemberUpdateEventInfo {
+	pub fn new(user_id: String) -> Self { 
+		return Self { user_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct RoleCreateEventInfo {
+	pub role_id: String,
+}
+
+impl RoleCreateEventInfo {
+	pub fn new(role_id: String) -> Self { 
+		return Self { role_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct RoleDeleteEventInfo {
+	pub role_id: String,
+}
+
+impl RoleDeleteEventInfo {
+	pub fn new(role_id: String) -> Self { 
+		return Self { role_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct RoleUpdateEventInfo {
+	pub role_id: String,
+}
+
+impl RoleUpdateEventInfo {
+	pub fn new(role_id: String) -> Self { 
+		return Self { role_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct ChannelCreateEventInfo {
+	pub channel_id: String,
+}
+
+impl ChannelCreateEventInfo {
+	pub fn new(channel_id: String) -> Self { 
+		return Self { channel_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct ChannelDeleteEventInfo {
+	pub channel_id: String,
+}
+
+impl ChannelDeleteEventInfo {
+	pub fn new(channel_id: String) -> Self { 
+		return Self { channel_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct ChannelUpdateEventInfo {
+	pub channel_id: String,
+}
+
+impl ChannelUpdateEventInfo {
+	pub fn new(channel_id: String) -> Self { 
+		return Self { channel_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct VoiceUpdateEventInfo {
+	pub channel_id: String,
+	pub user_id: String,
+}
+
+impl VoiceUpdateEventInfo {
+	pub fn new(channel_id: String, user_id: String) -> Self { 
+		return Self { channel_id, user_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct GuildUpdateEventInfo {}
+
+impl GuildUpdateEventInfo {
+	pub fn new() -> Self { 
+		return Self {};
+	}
+}
+
+#[derive(Debug)]
+pub struct ReactionAddEventInfo {
+	pub channel_id: String,
+	pub message_id: String,
+	pub user_id: String,
+	pub reaction_id: String,
+}
+
+impl ReactionAddEventInfo {
+	pub fn new(channel_id: String, message_id: String, user_id: String, reaction_id: String) -> Self { 
+		return Self { channel_id, message_id, user_id, reaction_id };
+	}
+}
+
+#[derive(Debug)]
+pub struct ReactionRemoveEventInfo {
+	pub channel_id: String,
+	pub message_id: String,
+	pub user_id: String,
+	pub reaction_id: String,
+}
+
+impl ReactionRemoveEventInfo {
+	pub fn new(channel_id: String, message_id: String, user_id: String, reaction_id: String) -> Self { 
+		return Self { channel_id, message_id, user_id, reaction_id };
 	}
 }

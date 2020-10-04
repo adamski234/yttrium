bb253262b46ee202dc10fd0a3620328539fe344b
adamski234 <adamski234@protonmail.com>
2020-10-04 19:43:39 +0200

    Added serenity context to the environment

--- key_base/src/environment.rs
+++ key_base/src/environment.rs
@@ -2,18 +2,18 @@ use crate::embed;
 use crate::databases;
 #[path = "./events.rs"] pub mod events;
 
-#[derive(Debug)]
-pub struct Environment {
+pub struct Environment<'a> {
 	pub embed: Option<embed::Embed>,
 	pub database_manager: databases::DatabaseManager,
 	pub guild_id: String,
 	pub target: String, //Default is the channel in which the bot was triggered
 	pub attachments: Vec<String>, //For the attachments to send in url form
 	pub event_info: events::EventType,
+	pub discord_context: &'a mut serenity::client::Context,
 }
 
-impl Environment {
-	pub fn new(event_info: events::EventType, guild_id: String) -> Self {
+impl<'a> Environment<'a> {
+	pub fn new(event_info: events::EventType, guild_id: String, context: &'a mut serenity::client::Context) -> Self {
 		return Self {
 			embed: None,
 			target: String::new(),
@@ -21,6 +21,7 @@ impl Environment {
 			guild_id: guild_id,
 			attachments: Vec::new(),
 			event_info: event_info,
+			discord_context: context,
 		};
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,11 @@ use std::collections::HashMap;
 #[path ="./interpreter.rs"] pub mod interpreter;
 
 
-pub fn run_ars_string(ars_string: String, key_list: &HashMap<String, Box<dyn key_base::Key>>, event_info: key_base::environment::events::EventType, guild_id: String) -> Result<ResultAndWarnings, errors_and_warns::Error> {
+pub fn run_ars_string(ars_string: String, key_list: &HashMap<String, Box<dyn key_base::Key>>, environment: key_base::environment::Environment) -> Result<ResultAndWarnings, errors_and_warns::Error> {
 	match tree_creator::create_ars_tree(ars_string, key_list) {
 		Ok(tree) => {
 			return Ok(ResultAndWarnings {
-				result: run_ars_tree(tree.tree, key_list, event_info, guild_id),
+				result: run_ars_tree(tree.tree, key_list, environment),
 				warnings: tree.warnings,
 			});
 		}
@@ -22,8 +22,8 @@ pub fn run_ars_string(ars_string: String, key_list: &HashMap<String, Box<dyn key
 	}
 }
 
-pub fn run_ars_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<String, Box<dyn key_base::Key>>, event_info: key_base::environment::events::EventType, guild_id: String) -> interpreter::InterpretationResult {
-	return interpreter::interpret_tree(tree, key_list, key_base::environment::Environment::new(event_info, guild_id));
+pub fn run_ars_tree(tree: Vec<tree_creator::TreeNode>, key_list: &HashMap<String, Box<dyn key_base::Key>>, environment: key_base::environment::Environment) -> interpreter::InterpretationResult {
+	return interpreter::interpret_tree(tree, key_list, environment);
 }
 
 #[derive(Debug)]

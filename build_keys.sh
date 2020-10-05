7220c315f54bcfd8b9acf3913765d4404cb47c2f
adamski234 <adamski234@protonmail.com>
2020-10-05 19:57:00 +0200

    Updated the key build script

--- build_keys.sh
+++ build_keys.sh
@@ -1,3 +1,12 @@
-#!/bin/zsh
-cargo build --all
-cp target/debug/libstd*.so keys/
+#!/bin/sh
+for dir in std_*
+do
+	cargo build -p $dir $@
+	# Check for `release` argument to determine the directory to take the files from
+	if [[ "$@" == *"--release"* ]]
+	then
+		cp ./target/release/lib$dir.so ./keys
+	else
+		cp ./target/debug/lib$dir.so ./keys
+	fi
+done

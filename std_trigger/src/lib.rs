d99cdc9e2e42b58180fc9f28e1fdd1c96dba0ff1
adamski234 <adamski234@protonmail.com>
2020-10-19 19:44:23 +0200

    Added the trigger key

---
+++ std_trigger/src/lib.rs
@@ -0,0 +1,81 @@
+#![allow(non_camel_case_types)]
+#![allow(clippy::needless_return)]
+#![deny(clippy::implicit_return)]
+
+use key_base::environment::events;
+#[no_mangle]
+pub fn key_create() -> *mut dyn key_base::Key {
+	let key_info = key_base::KeyInfo {
+		name: String::from("trigger"),
+		parameters_required: vec![0],
+	};
+	return Box::into_raw(Box::new(std_trigger {
+		info: key_info,
+		function: key_function,
+	}));
+}
+
+struct std_trigger {
+	pub info: key_base::KeyInfo,
+	pub function: fn(parameter: &Vec<String>, environment: &mut key_base::environment::Environment) -> String,
+}
+
+impl key_base::Key for std_trigger {
+	fn get_key_info(&self) -> &key_base::KeyInfo {
+		return &self.info;
+	}
+
+	fn get_key_function(&self) -> fn(parameter: &Vec<String>, environment: &mut key_base::environment::Environment) -> String {
+		return self.function;
+	}
+}
+
+fn key_function(_parameter: &Vec<String>, environment: &mut key_base::environment::Environment) -> String {
+	match &environment.event_info {
+		events::EventType::Message(event) => {
+			return event.trigger.clone();
+		}
+		events::EventType::Default => {
+			return String::from("EventType::Default");
+		}
+		events::EventType::MemberJoin(_) => {
+			return String::from("EventType::MemberJoin");
+		}
+		events::EventType::MemberLeave(_) => {
+			return String::from("EventType::MemberLeave");
+		}
+		events::EventType::MemberUpdate(_) => {
+			return String::from("EventType::MemberUpdate");
+		}
+		events::EventType::RoleCreate(_) => {
+			return String::from("EventType::RoleCreate");
+		}
+		events::EventType::RoleDelete(_) => {
+			return String::from("EventType::RoleDelete");
+		}
+		events::EventType::RoleUpdate(_) => {
+			return String::from("EventType::RoleUpdate");
+		}
+		events::EventType::ChannelCreate(_) => {
+			return String::from("EventType::ChannelCreate");
+		}
+		events::EventType::ChannelDelete(_) => {
+			return String::from("EventType::ChannelDelete");
+		}
+		events::EventType::ChannelUpdate(_) => {
+			return String::from("EventType::ChannelUpdate");
+		}
+		events::EventType::GuildUpdate(_) => {
+			return String::from("EventType::GuildUpdate");
+		}
+		events::EventType::VoiceUpdate(_) => {
+			return String::from("EventType::VoiceUpdate");
+		}
+		events::EventType::ReactionAdd(_) => {
+			return String::from("EventType::ReactionAdd");
+		}
+		events::EventType::ReactionRemove(_) => {
+			return String::from("EventType::ReactionRemove");
+		}
+	}
+}

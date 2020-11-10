d57ad21b1940e2ac829e26d644d0636be1e510e1
adamski234 <adamski234@protonmail.com>
2020-11-10 11:48:17 +0100

    Actually implemented the user key

--- std_user/src/lib.rs
+++ std_user/src/lib.rs
@@ -1,31 +1,31 @@
 #![allow(clippy::needless_return)]
 #![deny(clippy::implicit_return)]
 use key_base::environment::events::*;
-use serenity::model::id::{ChannelId, MessageId};
+use serenity::model::id::{UserId, GuildId};
 #[no_mangle]
 pub fn key_create() -> *mut dyn key_base::Key {
 	/*
 	Parameters:
-	Required, chooses the kind of mention to use, possible values: channels, users, roles
-	Required, offset, starting with 0
+	Optional, chooses information to return, possible values: id, nickname, username, avatar, discriminator
+	Optional, user ID to target
 	*/
 	let key_info = key_base::KeyInfo {
 		name: String::from("user"),
 		parameters_required: vec![2],
 	};
-	return Box::into_raw(Box::new(std_mention {
+	return Box::into_raw(Box::new(std_user {
 		info: key_info,
 		function: key_function,
 	}));
 }
 
 #[allow(non_camel_case_types)]
-struct std_mention {
+struct std_user {
 	pub info: key_base::KeyInfo,
 	pub function: fn(parameter: &[String], environment: &mut key_base::environment::Environment) -> String,
 }
 
-impl key_base::Key for std_mention {
+impl key_base::Key for std_user {
 	fn get_key_info(&self) -> &key_base::KeyInfo {
 		return &self.info;
 	}
@@ -36,36 +36,68 @@ impl key_base::Key for std_mention {
 }
 
 fn key_function(parameter: &[String], environment: &mut key_base::environment::Environment) -> String {
-	let message_id;
-	let channel_id;
-	match &environment.event_info {
-		EventType::Message(event) => {
-			message_id = MessageId::from(event.message_id.parse::<u64>().unwrap());
-			channel_id = ChannelId::from(event.channel_id.parse::<u64>().unwrap());
-		}
-		EventType::ReactionAdd(event) => {
-			message_id = MessageId::from(event.message_id.parse::<u64>().unwrap());
-			channel_id = ChannelId::from(event.channel_id.parse::<u64>().unwrap());
-		}
-		EventType::ReactionRemove(event) => {
-			message_id = MessageId::from(event.message_id.parse::<u64>().unwrap());
-			channel_id = ChannelId::from(event.channel_id.parse::<u64>().unwrap());
-		}
-		_ => {
-			return String::new();
+	let guild_id = GuildId::from(environment.guild_id.parse::<u64>().unwrap());
+	let user_id;
+	if parameter.len() == 2 {
+		user_id = UserId::from(parameter[1].parse::<u64>().unwrap());
+	} else {
+		match &environment.event_info {
+			EventType::MemberJoin(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::MemberLeave(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::MemberUpdate(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::Message(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::VoiceUpdate(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::ReactionAdd(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			EventType::ReactionRemove(event) => {
+				user_id = UserId::from(event.user_id.parse::<u64>().unwrap());
+			}
+			_ => {
+				return String::new();
+			}
 		}
 	}
-	let index: usize = parameter[1].parse().unwrap();
-	let message = futures::executor::block_on(environment.discord_context.cache.message(channel_id, message_id)).unwrap();
+	let user = futures::executor::block_on(environment.discord_context.cache.member(guild_id, user_id)).unwrap();
 	match parameter[0].as_str() {
-		"channels" => {
-			return message.mention_channels[index].id.to_string();
+		"id" => {
+			return user.user.id.to_string();
 		}
-		"users" => {
-			return message.mentions[index].id.to_string();
+		"nickname" => {
+			match user.nick {
+				Some(nick) => {
+					return nick;
+				}
+				None => {
+					return user.user.name;
+				}
+			}
 		}
-		"roles" => {
-			return message.mention_roles[index].to_string();
+		"username" => {
+			return user.user.name;
+		}
+		"avatar" => {
+			match user.user.avatar_url() {
+				Some(url) => {
+					return url;
+				}
+				None => {
+					return String::new();
+				}
+			}
+		}
+		"discriminator" => {
+			return user.user.discriminator.to_string();
 		}
 		_ => {
 			return String::new();

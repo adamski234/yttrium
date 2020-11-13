e9ddeafddb221091dcaad24beee5bbf18a208db2
adamski234 <adamski234@protonmail.com>
2020-11-13 11:02:35 +0100

    Added a text key replacing two other keys

--- std_text/src/lib.rs
+++ std_text/src/lib.rs
@@ -4,27 +4,27 @@
 pub fn key_create() -> *mut dyn key_base::Key {
 	/*
 	Parameters:
-	Required: chooses the pattern format, either glob or regex
-	Required: Pattern
-	Required: String to match against
+	Required, operation to perform. Possible values: equals, contains, starts_with, ends_with, regex, glob
+	Required, text to check
+	Required, text to check against. Regex if parameter 0 is `regex`, glob if `glob`, etc.
 	*/
 	let key_info = key_base::KeyInfo {
-		name: String::from("match"),
+		name: String::from("text"),
 		parameters_required: vec![3],
 	};
-	return Box::into_raw(Box::new(std_match {
+	return Box::into_raw(Box::new(std_text {
 		info: key_info,
 		function: key_function,
 	}));
 }
 
 #[allow(non_camel_case_types)]
-struct std_match {
+struct std_text {
 	pub info: key_base::KeyInfo,
 	pub function: fn(parameter: &[String], environment: &mut key_base::environment::Environment) -> String,
 }
 
-impl key_base::Key for std_match {
+impl key_base::Key for std_text {
 	fn get_key_info(&self) -> &key_base::KeyInfo {
 		return &self.info;
 	}
@@ -35,10 +35,23 @@ impl key_base::Key for std_match {
 }
 
 fn key_function(parameter: &[String], _environment: &mut key_base::environment::Environment) -> String {
+	let result;
 	match parameter[0].as_str() {
+		"equals" => {
+			result = parameter[1] == parameter[2];
+		}
+		"contains" => {
+			result = parameter[1].contains(&parameter[2]);
+		}
+		"starts_with" => {
+			result = parameter[1].starts_with(&parameter[2]);
+		}
+		"ends_with" => {
+			result = parameter[1].ends_with(&parameter[2]);
+		}
 		"regex" => {
 			let matcher;
-			match regex::Regex::new(&parameter[1]) {
+			match regex::Regex::new(&parameter[2]) {
 				Ok(value) => {
 					matcher = value;
 				}
@@ -46,15 +59,11 @@ fn key_function(parameter: &[String], _environment: &mut key_base::environment::
 					return String::from("0");
 				}
 			}
-			if matcher.is_match(&parameter[2]) {
-				return String::from("1");
-			} else {
-				return String::from("0");
-			}
+			result = matcher.is_match(&parameter[1]);
 		}
 		"glob" => {
 			let matcher;
-			match glob::Pattern::new(&parameter[1]) {
+			match glob::Pattern::new(&parameter[2]) {
 				Ok(value) => {
 					matcher = value;
 				}
@@ -62,14 +71,11 @@ fn key_function(parameter: &[String], _environment: &mut key_base::environment::
 					return String::from("0");
 				}
 			}
-			if matcher.matches(&parameter[2]) {
-				return String::from("1");
-			} else {
-				return String::from("0");
-			}
+			result = matcher.matches(&parameter[1]);
 		}
 		_ => {
-			return String::new();
+			result = false;
 		}
 	}
+	return String::from(if result { "1" } else { "0" });
 }
